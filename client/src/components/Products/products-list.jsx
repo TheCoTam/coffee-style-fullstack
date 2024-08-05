4d6e72e721d57368dbf0c80f4d3aaab25fbcca86
TheCoTam <hoangnguyenanhtuyen@gmail.com>
2024-08-05 18:25:14 +0700

    ghép api vào trang sản phẩm

--- client/src/components/Products/products-list.jsx
+++ client/src/components/Products/products-list.jsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
+
 import MoreProductsItem from "@/components/home/more-products-item";
 import SkeletonItem from "./SkeletonItem";
 import Slider from "./slider";
-import { more_products_data as products } from "@/data/pseudo-data-home";
 
 const ProductsList = () => {
+  const url = import.meta.env.VITE_BACKEND_URL;
+  const [products, setProducts] = useState([]);
+  const [searchParams] = useSearchParams();
+  const category = searchParams.get("category") || "all";
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await axios.get(url + "/api/mug/all/" + category);
+
+        if (response.data.success) {
+          setProducts(response.data.data);
+        } else {
+          toast.error(response.data.message);
+        }
+      } catch (error) {
+        console.log("[products-list]", error);
+        toast.error("Something went wrong");
+      }
+    }
+
+    fetchProducts();
+  }, [category]);
+
   if (!products || products.length === 0) {
     return (
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 w-full px-4 md:px-0  md:w-auto">
@@ -15,7 +43,7 @@ const ProductsList = () => {
   }
   return (
     <div className="flex flex-col items-center gap-10 w-full">
-      <Slider />
+      {category === "all" && <Slider />}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 lg:gap-5 px-3">
         {products.map((item, index) => (
           <MoreProductsItem key={index} {...item} />
---
+++ client/src/data/product-filter-menu.js
@@ -0,0 +1,31 @@
+export const PRODUCT_FILTER_MENU = [
+  {
+    name: "Our Products",
+    value: "all",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    name: "Coffee Mugs",
+    value: "coffee-mug",
+    description:
+      "Needless to say it’s very important, content is king and people are beginning to understand that.",
+  },
+  {
+    name: "Others",
+    value: "others",
+    description:
+      "eedless to say it’s very important, content is king and people are beginning to understand that.",
+  },
+  {
+    name: "Premium",
+    value: "premium",
+    description:
+      "However, back over in reality some project schedules and budgets don’t allow for web copy to be written before the design phase.",
+  },
+  {
+    name: "Tea Mugs",
+    value: "tea-mugs",
+    description:
+      "However, back over in reality some project schedules and budgets don’t allow for web copy to be written before the design phase.",
+  },
+];
